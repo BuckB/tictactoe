bdf847df8e29a36eb8ea25e5eec16f2426fc4854
Bruno Buck <b.brigagao@gmail.com>
2021-11-08 01:08:51 -0300

    Test for the first requirement Mark is outside the X axis

--- app/src/main/java/tictactoe/App.java
+++ app/src/main/java/tictactoe/App.java
@@ -1,4 +1,10 @@
 package tictactoe;
 
 public class App {
+
+    public void play(int x, int y) {
+        if (x < 1 || x > 3) {
+            throw new RuntimeException("Invalid mark outside the board");
+        }
+    }
 }
