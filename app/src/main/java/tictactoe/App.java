9225a103d076f935b882d2269c8b7a202274b09d
Bruno Buck <b.brigagao@gmail.com>
2022-06-07 11:31:08 -0300

    Refactor and new methods
    added playCounter and winCondition.
    changed access modifiers for most methods (from public to private)
    added win checker method.
    added draw checker method.
    added verification for win conditions: lines, columns and diagonals.

--- app/src/main/java/tictactoe/App.java
+++ app/src/main/java/tictactoe/App.java
@@ -4,43 +4,82 @@ public class App {
 
     private Character[][] board = new Character[3][3];
     private char lastPlayer = '\0';
+    private int playCounter = 0;
+    private static int winCondition = 3;
 
     public String play(int x, int y) {
         validateMove(x);
         validateMove(y);
         lastPlayer = nextPlayer();
         validateSpace(x, y, lastPlayer);
-        if (isWin()) {
+        if (playCounter > 4 && isWin()) {
             return lastPlayer + " is the winner.";
+        } else if (isDraw()){
+            return "It's a draw.";
         }
         return "No winner.";
     }
 
-    public void validateMove(int position) {
+    private void validateMove(int position) {
         if (position < 0 || position > 2) {
             throw new RuntimeException("Invalid move outside the board");
         }
     }
 
-    public void validateSpace(int x, int y, char lastPlayer) {
+    private void validateSpace(int x, int y, char lastPlayer) {
         if (board[x][y] != null) {
             throw new RuntimeException("Invalid move, this spot is occupied");
         }
         board[x][y] = lastPlayer;
+        playCounter++;
     }
 
     public char nextPlayer() {
         return lastPlayer == 'X' ? 'O' : 'X';
     }
 
-    public boolean isWin() {
-        for (int index = 0; index < 3; index++) {
-            if (board[index][0] == lastPlayer &&
-                    board[index][1] == lastPlayer &&
-                    board[index][2] == lastPlayer) {
+    private boolean isWin() {
+        if (verifyDiagonals()) return true;
+        if (verifyLines()) return true;
+        if (verifyColumns()) return true;
+        return false;
+    }
+
+    private boolean verifyLines() {
+        for (int index = 0; index < winCondition; index++) {
+            if (board[index][0] != null && board[index][1] != null && board[index][2] != null &&
+                    board[index][0] + board[index][1] + board[index][2] == lastPlayer * winCondition) {
                 return true;
             }
         }
         return false;
     }
+
+    private boolean verifyColumns() {
+        for (int index = 0; index < winCondition; index++) {
+            if (board[0][index] != null && board[1][index] != null && board[2][index] != null &&
+                    board[0][index] + board[1][index] + board[2][index] == lastPlayer * winCondition) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean verifyDiagonals() {
+        if (board[0][0] != null && board[1][1] != null && board[2][2] != null &&
+                board[0][0] + board[1][1] + board[2][2] == lastPlayer * winCondition) {
+            return true;
+        }
+        if (board[0][2] != null && board[1][1] != null && board[2][0] != null &&
+                board[0][2] + board[1][1] + board[2][0] == lastPlayer * winCondition) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isDraw() {
+        if(playCounter == 9) return true;
+        return false;
+    }
+
 }
